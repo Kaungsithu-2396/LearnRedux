d3ffc2f26b2a1096a68f47e9c4fdf604b646f6a1
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2023-06-03 20:25:40 +0630

    first day

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Todo from "./components/todo";
+function App() {
+    return (
+        <>
+            <h1>Welcome to Redux Lesson</h1>
+            <Todo />
+        </>
+    );
+}
+
+export default App;
